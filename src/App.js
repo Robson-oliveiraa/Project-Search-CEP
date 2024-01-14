29a2b8fe646248a4aae52d56bac2d649cdbe718d
Robson-oliveiraa <robsonpvh07@gmail.com>
2024-01-14 17:54:56 -0300

    Project Search-CEP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { BsSearch } from "react-icons/bs";
+import './App.css'
+import api from './service/api.js';
 
-function App() {
+export default function App() {
+
+
+  async function randomSearch() {
+    // 01310930/json
+
+    if (input === '') {
+      alert('Preencha com um CEP!')
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput('')
+
+    } catch {
+      alert('Valor invalido, digite novamente')
+      setInput('')
+    }
+  }
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="Container">
+      <h1 class="Title">Search CEP</h1>
+      <div class="ContainerInput">
+        <input
+          type="text"
+          placeholder="Digite seu CEP"
+          value={input}
+          onChange={(event) => setInput(event.target.value)} />
+        <button class="ButtonSearch"
+          onClick={randomSearch}>
+          <BsSearch />
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+        <main class="Main">
+          <h2>CPE: {cep.cep}</h2>
+          <span>Rua: {cep.logradouro}</span>
+          <span>Complemeno: {cep.complemento}</span>
+          <span>Bairro: {cep.bairro}</span>
+          <span>Cidade/UF: {cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
     </div>
   );
 }
-
-export default App;
